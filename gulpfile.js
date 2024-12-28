b08d74d6f6e372db994aa95bb5edd5ad833ef238
josue alves <joanancealves400@gmail.com>
2024-12-27 23:33:04 -0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
-function style(){
+function style() {
     return gulp.src("./src/styles/*.scss")
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest("./dist/main.css"));
 }
 
-exports.default = style;
+function images() {
+    return gulp.src("./src/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/images"));
+}
+
+exports.default = gulp.parallel(style, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(style))
 }
