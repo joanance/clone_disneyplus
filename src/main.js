a786e36a9581a9ddace6e622faec78959242bebe
josue alves <joanancealves400@gmail.com>
2025-01-23 01:23:13 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY ;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaroHeader();
+
+        }else {
+            exibeoHeader();
+        }
+
+    })
+
+    // seção de atração programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abAlvo = botao.target.dataset.tabButton;
@@ -11,7 +28,28 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    // seção faq acordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultaroHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions_item--is-open';
+    const elementopai = elemento.target.parentNode;
+
+    elementopai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
